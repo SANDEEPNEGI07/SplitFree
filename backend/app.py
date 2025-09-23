53bdb1e83a41d8fb17e4ed935c6b18b540033e1a
Sandeep Singh <sandeepnegi5898@gmail.com>
2025-09-23 23:31:39 +0530

    Move backend to dedicated folder

--- backend/app.py
+++ backend/app.py
@@ -65,7 +65,7 @@ def create_app(db_url = None):
 
     api = Api(app)
 
-    app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET")
+    app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
     jwt = JWTManager(app)
 
     @jwt.token_in_blocklist_loader
